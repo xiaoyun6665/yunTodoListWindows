3fd1577423d0fbd70ff5f3ff281a11e5c6abeefd
云孜微殇 <1963945679@qq.com>
2022-05-24 00:45:54 +0800

    feat: Main page

---
+++ packages/renderer/src/pages/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <HelloWorld  msg="Hello Vue 3 + TypeScript + Vite" />
+    1
+</template>
+
+<script setup lang="ts">
+import HelloWorld  from '../components/HelloWorld.vue'
+
+
+</script>
+
+<style>
+
+</style>
